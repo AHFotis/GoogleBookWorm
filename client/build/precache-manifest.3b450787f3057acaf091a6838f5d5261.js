e3251e203f4c21f2b86b86c762962de3c1d75f92
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-05-12 13:42:00 -0500

    Ran yarn build

---
+++ client/build/precache-manifest.3b450787f3057acaf091a6838f5d5261.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160baeb3ba0d8f8f360a70aa48af504b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e9a2050821995b3c290",
+    "url": "/static/css/main.392b4af4.chunk.css"
+  },
+  {
+    "revision": "2f5dee113807bfad264b",
+    "url": "/static/js/2.f9168adb.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.f9168adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9a2050821995b3c290",
+    "url": "/static/js/main.f5aadffd.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "f37d5f5879c901d988b45ba7a2aa3ba1",
+    "url": "/static/media/stack.f37d5f58.jpg"
+  }
+]);
