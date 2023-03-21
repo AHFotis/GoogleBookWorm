e44c4561941502e3811a157576b2bb05977bfd8b
Anna Fotis <annahickeyfotis@gmail.com>
2023-03-20 20:26:32 -0500

    fix react scripts part deux

--- client/src/components/SaveBtn/index.js
+++ client/src/components/SaveBtn/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function SaveBtn(props) {
-  // console.log(props);
   return (
       <button className="save-btn btn btn-info" tabIndex="0" onClick={() => props.handleBookSave(props.bookData)} style={{ float: "right", marginBottom: 10 }}>
         {props.children} 
